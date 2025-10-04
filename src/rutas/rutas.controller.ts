e5071c0c60f14285bcac8d5abfba4852bb2498e1
Bryan Rojas <bryan.rojas-ext@promart.pe>
2025-10-04 11:40:26 -0500

    se modifica endpoint de listado de rutas

--- src/rutas/rutas.controller.ts
+++ src/rutas/rutas.controller.ts
@@ -1,18 +1,34 @@
-import { Controller, Get, Param,Query  } from '@nestjs/common';
+import { Controller, Get, Param,Query,BadRequestException,ParseEnumPipe } from '@nestjs/common';
 import { RutasService } from './rutas.service';
-
+import dayjs from 'dayjs';
+export enum EstadoRuta {
+  PENDIENTE = 'PENDIENTE',
+  FINALIZADO = 'FINALIZADO'
+}
+export const EstadoRutaMap: Record<EstadoRuta, number> = {
+  [EstadoRuta.PENDIENTE]: 0,
+  [EstadoRuta.FINALIZADO]: 1,
+};
 @Controller('rutas')
 export class RutasController {
   constructor(private readonly rutasService: RutasService) {}
 
   @Get('tecnico')
-  async obtenerRutasTecnico(
-    @Query('id') id: string,
-    @Query('fecha') fecha: string,
-    @Query('estado') estado: number
-  ) {
-    return this.rutasService.obtenerRutasTecnico(String(id), fecha,estado);
+async obtenerRutasTecnico(
+  @Query('usuario') usuario: string,
+  @Query('fecha') fecha: string
+) {
+  const fechaValida = dayjs(fecha, 'YYYY-MM-DD', true);
+
+  if (!fechaValida.isValid()) {
+    throw new BadRequestException('Formato de fecha inválido. Usa YYYY-MM-DD');
   }
+
+  return this.rutasService.obtenerRutasTecnico(
+    usuario,
+    fechaValida.format('YYYY-MM-DD')
+  );
+}
   @Get('estados')
   async obtenerEstados() {
     return this.rutasService.obtenerEstados();
--- src/rutas/rutas.service.ts
+++ src/rutas/rutas.service.ts
@@ -1,11 +1,24 @@
 import { Injectable, Inject,InternalServerErrorException  } from '@nestjs/common';
 import { Pool } from 'pg';
 export interface Ruta {
-  id_ruta: number;
-  fecha: string; 
-  estado: string;
-  tecnico: string;
-  direccion:string;
+  id_ruta: number;                          // r.id_ruta
+  fecha: string;                            // r.fecha
+  estado_ruta: number;                      // r.id_estado_ruta
+  estado_servicio_desc: string;             // e.estado_desc
+  subestado_servicio_desc?: string | null;  // s.subestado_desc (puede ser null)
+  tecnico: string;                          // t.nombre
+  direccion: string;                        // c.direccion
+  distrito: string;                         // c.distrito
+  telefono: string;                         // c.telefono
+  nombre_cliente: string;                   // c.nombre_cliente
+  referencia?: string | null;               // c.referencia
+  id_pedido: number;                        // p.id_pedido
+  sku_producto: string;                     // p.sku_producto
+  sku_producto_desc: string;                // p.sku_producto_desc
+  sku_servicio: string;                     // p.sku_servicio
+  turno: string;                            // p.turno
+  fecha_servicio: string;                   // alias r.fecha AS fecha_servicio
+  observacion_servicio?: string | null;     // p.observacion_servicio
 }
 export interface Estado { idEstado: number; estadoDesc: string; }
 
@@ -18,24 +31,24 @@ export interface SubEstado {
 export class RutasService {
   constructor(@Inject('PG_POOL') private pool: Pool) {}
 
-  async obtenerRutasTecnico(id_tecnico: string, fecha: string,idEstado: number): Promise<Ruta[]> {
+  async obtenerRutasTecnico(id_tecnico: string, fecha: string): Promise<Ruta[]> {
     const query = `
-     SELECT r.id_ruta, r.fecha,e.estado_desc,s.subestado_desc , t.nombre AS tecnico,c.direccion,c.distrito,c.telefono,c.nombre_cliente,c.referencia, p.id_pedido,
+          SELECT r.id_ruta, r.fecha,r.id_estado_ruta as estado_ruta, e.estado_desc as estado_servicio_desc,s.subestado_desc as subestado_servicio_desc, t.nombre AS tecnico,c.direccion,c.distrito,c.telefono,c.nombre_cliente,c.referencia, p.id_pedido,
   p.sku_producto, p.sku_producto_desc,p.sku_servicio, p.sku_servicio,p.turno,r.fecha as fecha_servicio,
   p.observacion_servicio
         FROM rutas r
        inner JOIN tecnicos t ON r.id_tecnico = t.id_tecnico
-      INNER JOIN ruta_pedidos rt on r.id_ruta=rt.id_ruta
+      inner JOIN ruta_pedidos rt on r.id_ruta=rt.id_ruta
       inner join pedidos p on rt.id_pedido=p.id_pedido
       inner join clientes c on p.id_cliente=c.id_cliente
-	  inner join estados e on r.id_estado=e.id_estado
-      left join sub_estados s on r.id_subestado=s.id_subestado
+	  left join estados e on p.id_estado=e.id_estado
+      left join sub_estados s on p.id_subestado=s.id_subestado
         WHERE t.usuario = $1
           AND DATE(r.fecha) =  $2
-		  and r.id_estado= $3
         ORDER BY r.fecha DESC;
+
     `;
-    const result = await this.pool.query(query,[id_tecnico,fecha,idEstado]);
+    const result = await this.pool.query(query,[id_tecnico,fecha]);
     return result.rows as Ruta[];
   }
     async obtenerEstados(): Promise<Estado[]> {
